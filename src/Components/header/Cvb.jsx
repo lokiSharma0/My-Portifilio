dd8be0dd1db1768b2f8412d4a91b3d3e343b3465
Lokendra Sharma <lorenzsharma@gmail.com>
2023-09-01 17:17:24 +1000

    newwchnages

--- src/Components/header/Cvb.jsx
+++ src/Components/header/Cvb.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import CV from '../../assets/Lokendra_Resume.pdf'
-import './header.css'
- const Cvb = () => {
+import React from "react";
+// import CV from "../../assets/lokendraSharmaSD_Resume.pdf";
+import CV from "../../assets/Lokendra_Sharma.pdf";
+import "./header.css";
+const Cvb = () => {
   return (
-    <div className='cvb-main-div'>
-        <a className='btn' href={CV} download>Download CV</a>
-        <a className='btn' href='#contact'>Have Talk </a>
+    <div className="cvb-main-div">
+      <a className="btn" href={CV} download>
+        Download CV
+      </a>
+      <a className="btn" href="#contact">
+        Have Talk{" "}
+      </a>
     </div>
-  )
-}
-export default Cvb
+  );
+};
+export default Cvb;
